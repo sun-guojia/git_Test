8d8c475b3f0741d24b98a978be1de571560bb59d
sgj <1590004791@qq.com>
2023-03-21 09:57:53 +0800

    first commit

---
+++ src/main/java/com/sgj/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.sgj.git;
+
+public class GitTest {
+/**
+ * @description:
+ * @author: xz
+ * @create: 2023-03-21 09:36
+ */
+public static void main(String[] args) {
+    System.out.println("hello git idea");
+}
+}
