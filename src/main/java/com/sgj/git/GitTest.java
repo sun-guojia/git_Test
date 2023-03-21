8bbfed5e80dce8920088fcb52e9c690f008c7513
sgj <1590004791@qq.com>
2023-03-21 11:52:06 +0800

    conflict commit2

--- src/main/java/com/sgj/git/GitTest.java
+++ src/main/java/com/sgj/git/GitTest.java
@@ -10,5 +10,6 @@ public static void main(String[] args) {
     System.out.println("hello git idea");
     System.out.println("第二次提交");
     System.out.println("hotfix");
+    System.out.println("合并冲突2");
     }
 }
